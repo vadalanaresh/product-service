a94d7ed81afaab121c929eb7cb519b458ad78658
Naresh.Vadala <vadalanaresh@gmail.com>
2024-04-15 08:41:24 +0530

    Product service api

---
+++ src/main/java/com/exampleproductserviec/prodcutservice/ProdcutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exampleproductserviec.prodcutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdcutServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exampleproductserviec/prodcutservice/ProdcutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleproductserviec.prodcutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
